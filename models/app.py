3d1e1c49394b3d18d6e52e66d1c8df46bb7a9a5b
rami albasha <rami.albasha@inrae.fr>
2022-09-28 23:40:02 +0200

    shiny app alpha completed

---
+++ models/app.py
@@ -0,0 +1,231 @@
+# Adapted from https://shinylive.io/py/examples/#orbit-simulation
+
+from pathlib import Path
+
+from matplotlib import pyplot
+from shiny import App, reactive, render, ui
+
+import sim_funcs
+
+pyplot.ioff()
+
+path_root = Path(__file__).parent
+path_data = path_root / 'data'
+path_static = path_root / "static"
+
+
+def panel_box(*args, **kwargs):
+    return ui.div(
+        ui.div(*args, class_="card-body"),
+        **kwargs,
+        class_="card mb-3",
+    )
+
+
+app_ui = ui.page_fluid(
+    {"class": "p-4"},
+    ui.row(
+        ui.column(
+            2,
+            ui.img(src="logos_mesr_occitanie.png", style="width: 100%; max-width: 250px;"),
+        ),
+        ui.column(
+            1,
+            ui.img(src="logo_fds_noir_rouge.png", style="width: 100%; max-width: 250px; vertical-align:top"),
+        ),
+        ui.column(
+            2,
+            ui.img(src="logo_lepse.jpg", style="width: 100%; max-width: 250px;"),
+        ),
+        ui.column(
+            2,
+            ui.img(src="logo_instant_science.png", style="width: 50%; max-width: 250px;"),
+        ),
+    ),
+    ui.row(
+        ui.column(
+            3,
+            panel_box(
+                ui.row(
+                    ui.column(8,
+                              ui.input_select(
+                                  "canopy_1",
+                                  "Conduite 1",
+                                  {
+                                      "vsp": "Espalier bas",
+                                      "gdc": "Rideau simple",
+                                      "lyre": "Lyre",
+                                      "cordon": "Cordon libre",
+                                  },
+                              ),
+                              ),
+                    ui.column(4,
+                              ui.output_ui("display_canopy1"),
+                              )
+                ),
+                ui.row(
+                    ui.column(8,
+                              ui.input_select(
+                                  "canopy_2",
+                                  "Conduite 2",
+                                  {
+                                      "vsp": "Espalier bas",
+                                      "gdc": "Rideau simple",
+                                      "lyre": "Lyre",
+                                      "cordon": "Cordon libre",
+                                  },
+                              ),
+                              ),
+                    ui.column(4,
+                              ui.output_ui("display_canopy2"),
+                              ),
+                ),
+                ui.input_action_button("compare", "Comparer", class_="btn-primary w-100"),
+            ),
+            ui.output_plot("display_water_use_efficiency", height='300px'),
+            panel_box(
+                ui.input_numeric(id="hour", label="Heure simulée", value=12, min=9, max=15, step=1),
+                ui.input_select(
+                    id="mtg_prop",
+                    label="Propriété à afficher",
+                    choices={
+                        "gs": "Conductance stomatique",
+                        "An": "Photosynthèse nette",
+                        "psi_head": "Potentiel Xylémien",
+                        "Tlc": "Température",
+                        "Na": "Teneur en azote",
+                        "E": "Transpiration",
+                        "gb": "Conductance Couche lim.",
+                    }
+                ),
+                ui.input_action_button(id="plot_mtg", label="Afficher", class_="btn-primary w-100"),
+            )
+        ),
+        ui.column(
+            6,
+            ui.output_plot("display_whole_plant", height='600px'),
+            ui.output_plot("display_mtg_property", height='200px')
+        ),
+        ui.column(
+            3,
+            panel_box(
+                ui.row(
+                    ui.column(8,
+                              ui.input_select(
+                                  id="canopy_user",
+                                  label="Conduite 3",
+                                  choices={
+                                      "none": "aucun",
+                                      "vsp": "Espalier bas",
+                                      "gdc": "Rideau simple",
+                                      "lyre": "Lyre",
+                                      "cordon": "Cordon libre",
+                                  },
+                                  selected=None,
+                              ),
+                              ),
+                    ui.column(4,
+                              ui.output_ui("display_canopy_user"),
+                              )
+                ),
+                ui.input_slider("gs_min", "Conductance stomatique résiduelle\n(mmol/m2/s)", 0, 40, value=20),
+                ui.input_slider("psi_gs50", "Potentiel xylémien à 50% de la fermeture stomatique (MPa)", -4, 0,
+                                value=-1,
+                                step=0.1),
+                ui.input_slider("n", "Réponse au stress hydrique", 0, 5, value=4, step=0.1),
+                ui.input_action_button("run", "Lancer", class_="btn-primary w-100"),
+                ui.output_plot("display_stomatal_response")
+            ),
+        )
+    ),
+)
+
+
+def server(ui_input, ui_output, session):
+    @ui_output
+    @render.ui
+    def display_canopy1():
+        return ui.tags.img(src=f'icone_{ui_input.canopy_1()}.png', height='100pixel')
+
+    @ui_output
+    @render.ui
+    def display_canopy2():
+        return ui.tags.img(src=f'icone_{ui_input.canopy_2()}.png', height='100px')
+
+    @ui_output
+    @render.ui
+    def display_canopy_user():
+        return ui.tags.img(src=f'icone_{ui_input.canopy_user()}.png', height='100px')
+
+    @ui_output
+    @render.plot
+    @reactive.event(ui_input.compare, ui_input.run, ignore_init=True, ignore_none=False)
+    def display_whole_plant():
+        return sim_funcs.plot_whole_plant_gas_exchange(
+            path_ref=path_data / 'output_ref',
+            path_user=path_data / 'output_user',
+            training_system_1=ui_input.canopy_1(),
+            training_system_2=ui_input.canopy_2(),
+            training_system_user=ui_input.canopy_user(),
+            path_weather=path_data / 'weather.csv')
+
+    @ui_output
+    @render.plot
+    @reactive.event(ui_input.compare, ui_input.run, ignore_init=True, ignore_none=False)
+    def display_water_use_efficiency():
+        return sim_funcs.plot_water_use_efficiency(
+            path_ref=path_data / 'output_ref',
+            path_user=path_data / 'output_user',
+            training_system_1=ui_input.canopy_1(),
+            training_system_2=ui_input.canopy_2(),
+            training_system_user=ui_input.canopy_user())
+
+    pass
+
+    @ui_output
+    @render.plot
+    def display_stomatal_response():
+        return sim_funcs.plot_stomatal_reduction_coef(
+            psi50=ui_input.psi_gs50(),
+            gs0=ui_input.gs_min() / 1000,
+            n=ui_input.n())
+
+    @ui_output
+    @render.plot
+    @reactive.event(ui_input.plot_mtg, ignore_init=True, ignore_none=False)
+    def display_mtg_property():
+        return sim_funcs.display_mtg_property(
+            path_ref=path_data / 'output_ref',
+            path_user=path_data / 'output_user',
+            training_system_1=ui_input.canopy_1(),
+            training_system_2=ui_input.canopy_2(),
+            training_system_user=ui_input.canopy_user(),
+            hour=ui_input.hour(),
+            mtg_property=ui_input.mtg_prop()
+        )
+
+
+    @reactive.Effect
+    @reactive.event(ui_input.run, ignore_init=True, ignore_none=False)
+    def update_model_params():
+        sim_funcs.update_params(
+            path_default_params=path_data / 'params_default.json',
+            path_user_params=path_data / 'params.json',
+            stomatal_conductance_min=ui_input.gs_min(),
+            stomatal_psi50=ui_input.psi_gs50(),
+            steepness=ui_input.n())
+
+    @reactive.Effect
+    @reactive.event(ui_input.run, ignore_init=True, ignore_none=False)
+    def run_hydroshoot():
+        path_output = path_data / 'output_user'
+        path_output.mkdir(parents=True, exist_ok=True)
+        sim_funcs.run_hydroshoot(
+            path_project=path_data,
+            path_dir_preprocessed_inputs=path_data / 'preprocessed_inputs',
+            training_sys=ui_input.canopy_user(),
+            is_show=False,
+            path_output=path_output / 'time_series.csv')
+
+
+app = App(ui=app_ui, server=server, debug=True, static_assets=path_static)
--- models/sim_funcs.py
+++ models/sim_funcs.py
@@ -4,14 +4,13 @@ from json import load as load_json
 from pathlib import Path
 from pickle import load as load_pickle
 
-from hydroshoot import architecture, display, io, initialisation, model, constants
+from hydroshoot import architecture, display, io, initialisation, model, constants, exchange
 from matplotlib import pyplot, patches
 from matplotlib.dates import DateFormatter
 from openalea.mtg import traversal, mtg
-from openalea.plantgl.all import Scene
+from openalea.plantgl.all import Scene, Viewer
 from openalea.plantgl.all import surface as surf
 from pandas import read_csv, to_datetime
-
 
 pyplot.ioff()
 
@@ -50,6 +49,19 @@ def generate_grapevine_mtgs(path_dir_project: Path):
             scene=virtual_scene, name_index=f'{total_leaf_area:.2f}')
         architecture.save_mtg(
             g=g, scene=virtual_scene, file_path=path_output, filename=f'mtg{total_leaf_area:.2f}.pckl')
+    pass
+
+
+def generate_grapevine_mtgs2(path_dir_project: Path):
+    path_output = path_dir_project / 'preprocessed_inputs'
+    for training_sys in ('cordon', 'lyre', 'gdc', 'vsp'):
+        g = build_grapevine_mtg(path_digit_file=path_dir_project / f'digit_{training_sys}.csv')
+        virtual_scene = display.visu(g=g, def_elmnt_color_dict=True, scene=Scene(), view_result=True)
+        preprocess_inputs(
+            grapevine_mtg=g, path_project_dir=path_dir_project, psi_soil=-1, gdd_since_budbreak=1000.,
+            scene=virtual_scene, name_index=training_sys)
+        architecture.save_mtg(
+            g=g, scene=virtual_scene, file_path=path_output, filename=f'mtg{training_sys}.pckl')
     pass
 
 
@@ -99,27 +111,29 @@ def preprocess_inputs(grapevine_mtg: mtg.MTG, path_project_dir: Path, psi_soil: 
     pass
 
 
-def run_hydroshoot(path_dir_preprocessed_inputs: Path, path_output: Path, is_low_leaf_area: bool, is_show: bool = True):
-    leaf_area = 1.54 if is_low_leaf_area else 3.78
-    path_mtg_file = path_dir_preprocessed_inputs / f'mtg{leaf_area}.pckl'
+def run_hydroshoot(path_dir_preprocessed_inputs: Path, path_project: Path, path_output: Path, training_sys: str,
+                   is_show: bool = True, snap_shot_path: Path = False):
+    path_mtg_file = path_dir_preprocessed_inputs / f'mtg{training_sys}.pckl'
     with open(path_mtg_file, mode='rb') as f:
         g, _ = load_pickle(f)
 
-    if is_show:
-        # eval('%gui qt5')
-        for v in traversal.iter_mtg2(g, g.root):
-            architecture.vine_mtg_geometry(g, v)
-            architecture.vine_transform(g, v)
-        virtual_scene = display.visu(g=g, def_elmnt_color_dict=True, scene=Scene(), view_result=True)
-    else:
-        virtual_scene = None
+    # eval('%gui qt5')
+    for v in traversal.iter_mtg2(g, g.root):
+        architecture.vine_mtg_geometry(g, v)
+        architecture.vine_transform(g, v)
 
-    with open(path_dir_preprocessed_inputs / f'static{leaf_area}.json') as f:
+    virtual_scene = display.visu(g=g, def_elmnt_color_dict=True, scene=Scene(), view_result=False)
+    if is_show:
+        Viewer.display(virtual_scene)
+    if snap_shot_path is not None:
+        Viewer.saveSnapshot(str(snap_shot_path))
+
+    with open(path_dir_preprocessed_inputs / f'static{training_sys}.json') as f:
         static_inputs = load_json(f)
-    with open(path_dir_preprocessed_inputs / f'dynamic{leaf_area}.json') as f:
+    with open(path_dir_preprocessed_inputs / f'dynamic{training_sys}.json') as f:
         dynamic_inputs = load_json(f)
 
-    summary_output = model.run(g=g, wd=path_data, scene=virtual_scene,
+    summary_output = model.run(g=g, wd=path_project, scene=virtual_scene,
                                psi_soil=-0.5, gdd_since_budbreak=1000.,
                                form_factors=static_inputs['form_factors'],
                                leaf_nitrogen=static_inputs['Na'],
@@ -143,28 +157,112 @@ def display_whole_plant(path_output_low: Path, path_output_high: Path, path_weat
 
     axs[0, 0].plot(df_output_low.index, df_weather['Rg'], label='Incident', c='k')
     axs[0, 0].plot(df_output_low.index, df_output_low['Rg'], label='Absorbé (canopée clairsemée)', c='orange')
-    axs[0, 0].plot(df_output_low.index, df_output_high['Rg'], label='Absorbé (canopée dense)', c='red')
+    axs[0, 0].plot(df_output_high.index, df_output_high['Rg'], label='Absorbé (canopée dense)', c='red')
     axs[0, 0].set(ylabel='Rayonnement solaire\n' + '($\mathregular{W\/m^{-2}_{sol}}$)')
     axs[0, 0].legend(loc='center left', fontsize=8)
 
     axs[1, 0].plot(df_output_low.index, df_weather['Tac'], label='air', c='k')
     axs[1, 0].plot(df_output_low.index, df_output_low['Tleaf'], label='Canopée (clairsemée)', c='orange')
-    axs[1, 0].plot(df_output_low.index, df_output_high['Tleaf'], label='Canopée (dense)', c='red')
+    axs[1, 0].plot(df_output_high.index, df_output_high['Tleaf'], label='Canopée (dense)', c='red')
     axs[1, 0].set(ylabel='Température\n(°C)', xlabel='heure')
     axs[1, 0].legend(loc='lower right', fontsize=8)
 
     axs[0, 1].plot(df_output_low.index, df_output_low['An'] * CONV_CO2, c='orange', label='(clairsemée)')
-    axs[0, 1].plot(df_output_low.index, df_output_high['An'] * CONV_CO2, c='red', label='dense')
+    axs[0, 1].plot(df_output_high.index, df_output_high['An'] * CONV_CO2, c='red', label='dense')
     axs[0, 1].set(ylabel='\n'.join(('Photosynthèse nette', r'($\mathregular{g\/plant^{-1}}$)')))
     axs[0, 1].legend(loc='upper left', fontsize=8)
 
     axs[1, 1].plot(df_output_low.index, df_output_low['E'], c='orange', label='(clairsemée)')
-    axs[1, 1].plot(df_output_low.index, df_output_high['E'], c='red', label='dense')
+    axs[1, 1].plot(df_output_high.index, df_output_high['E'], c='red', label='dense')
     axs[1, 1].set(ylabel='\n'.join(('Transpiration', r'($\mathregular{g\/plant^{-1}}$)')), xlabel='heure')
     axs[1, 1].legend(loc='upper left', fontsize=8)
 
     axs[-1, -1].xaxis.set_major_formatter(DateFormatter('%H'))
     fig.tight_layout()
+    return fig
+
+
+def plot_whole_plant_gas_exchange(path_ref: Path, path_user: Path, training_system_1: str, training_system_2: str,
+                                  training_system_user: str, path_weather: Path) -> pyplot.Figure:
+    path_output_1 = path_ref / training_system_1 / f'time_series.csv'
+    path_output_2 = path_ref / training_system_2 / f'time_series.csv'
+    df_output_ref_1 = read_csv(path_output_1, sep=';', decimal='.', index_col='Unnamed: 0')
+    df_output_ref_2 = read_csv(path_output_2, sep=';', decimal='.', index_col='Unnamed: 0')
+    for df in (df_output_ref_1, df_output_ref_2):
+        df.index = to_datetime(df.index)
+
+    df_weather = read_csv(path_weather, sep=';', decimal='.', index_col='time')
+    df_weather.index = to_datetime(df_weather.index)
+    df_weather = df_weather.loc[df_output_ref_1.index, :]
+
+    fig, axs = pyplot.subplots(nrows=2, ncols=2, sharex='all')
+
+    axs[0, 0].plot(df_weather.index, df_weather['Rg'], label='Incident', c='k')
+    axs[0, 0].plot(df_output_ref_1.index, df_output_ref_1['Rg'], c='orange', label=training_system_1)
+    axs[0, 0].plot(df_output_ref_2.index, df_output_ref_2['Rg'], c='red', label=training_system_2)
+    axs[0, 0].set(ylabel='Rayonnement solaire\n' + '($\mathregular{W\/m^{-2}_{sol}}$)')
+
+    axs[0, 1].plot(df_output_ref_1.index, df_output_ref_1['An'] * CONV_CO2, c='orange', label=training_system_1)
+    axs[0, 1].plot(df_output_ref_2.index, df_output_ref_2['An'] * CONV_CO2, c='red', label=training_system_2)
+    axs[0, 1].set(ylabel='\n'.join(('Photosynthèse nette', r'($\mathregular{g\/plant^{-1}}$)')))
+
+    axs[1, 1].plot(df_output_ref_1.index, df_output_ref_1['E'], c='orange', label=training_system_1)
+    axs[1, 1].plot(df_output_ref_2.index, df_output_ref_2['E'], c='red', label=training_system_2)
+    axs[1, 1].set(ylabel='\n'.join(('Transpiration', r'($\mathregular{g\/plant^{-1}}$)')))
+
+    axs[1, 0].plot(df_output_ref_1.index, df_weather['Tac'], label='air', c='k')
+    axs[1, 0].plot(df_output_ref_1.index, df_output_ref_1['Tleaf'], label=training_system_1, c='orange')
+    axs[1, 0].plot(df_output_ref_2.index, df_output_ref_2['Tleaf'], label=training_system_2, c='red')
+    axs[1, 0].set(ylabel='Température\n(°C)', xlabel='heure')
+
+    if training_system_user != "none":
+        path_output_user = path_user / f'time_series.csv'
+        df_output_user = read_csv(path_output_user, sep=';', decimal='.', index_col='Unnamed: 0')
+        df_output_user.index = to_datetime(df_output_user.index)
+        kwargs = dict(label=f'{training_system_user} (usr)', c='blue')
+        axs[0, 0].plot(df_output_user.index, df_output_user['Rg'], **kwargs)
+        axs[0, 1].plot(df_output_user.index, df_output_user['An'] * CONV_CO2, **kwargs)
+        axs[1, 1].plot(df_output_user.index, df_output_user['E'], **kwargs)
+        axs[1, 0].plot(df_output_user.index, df_output_user['Tleaf'], **kwargs)
+        pass
+
+    axs[0, 0].legend()
+    axs[1, 0].legend(loc='lower right', fontsize=8)
+
+    for ax in axs[-1, :]:
+        ax.xaxis.set_major_formatter(DateFormatter('%H'))
+    fig.tight_layout()
+    return fig
+
+
+def plot_water_use_efficiency(path_ref: Path, path_user: Path, training_system_1: str, training_system_2: str,
+                              training_system_user: str) -> pyplot.Figure:
+    path_output_1 = path_ref / training_system_1 / f'time_series.csv'
+    path_output_2 = path_ref / training_system_2 / f'time_series.csv'
+    df_ref_1 = read_csv(path_output_1, sep=';', decimal='.', index_col='Unnamed: 0')
+    df_ref_1.index = to_datetime(df_ref_1.index)
+
+    df_ref_2 = read_csv(path_output_2, sep=';', decimal='.', index_col='Unnamed: 0')
+    df_ref_2.index = to_datetime(df_ref_2.index)
+
+    xticklebels = [training_system_1, training_system_2]
+    fig, ax = pyplot.subplots()
+    for x, df, c in ((0, df_ref_1, 'orange'), (1, df_ref_2, 'red')):
+        ax.bar(x, df.loc[df_ref_2.index, 'An'].sum() * CONV_CO2 / df['E'].sum(), facecolor=c)
+
+    if training_system_user != 'none':
+        df_user = read_csv(path_user / 'time_series.csv', sep=';', decimal='.', index_col='Unnamed: 0')
+        df_user.index = to_datetime(df_user.index)
+        ax.bar(2, df_user.loc[df_user.index, 'An'].sum() * CONV_CO2 / df_user['E'].sum(),
+               facecolor='blue')
+        xticklebels.append(training_system_user)
+
+    ax.set_xticks(range(len(xticklebels)))
+    ax.set_xticklabels(xticklebels)
+
+    ax.set(ylabel='\n'.join(["Efficience de l'utilisation de l'eau", "$\mathregular{g_{CO_2}\/g^{-1}_{H_2O}}$"]))
+    fig.tight_layout()
+
     return fig
 
 
@@ -220,7 +318,34 @@ def display_mtg_properties(path_output_low: Path, path_output_high: Path):
     pass
 
 
-def display_water_use_efficiency(path_output_low: Path, path_output_high: Path):
+def display_mtg_property(path_ref: Path, training_system_1: str, training_system_2: str, path_user: Path,
+                         training_system_user: str, hour: int, mtg_property: str) -> pyplot.Figure:
+    path_ref_1 = path_ref / training_system_1 / f'mtg20120801{hour:02d}0000.pckl'
+    path_ref_2 = path_ref / training_system_2 / f'mtg20120801{hour:02d}0000.pckl'
+    with open(path_ref_1, mode='rb') as f:
+        g_ref_1, _ = load_pickle(f)
+    with open(path_ref_2, mode='rb') as f:
+        g_ref_2, _ = load_pickle(f)
+
+    n_cols = 2
+    if training_system_user != 'none':
+        n_cols = 3
+        path_user = path_user / f'mtg20120801{hour:02d}0000.pckl'
+        with open(path_user, mode='rb') as f:
+            g_user, _ = load_pickle(f)
+
+    fig, axs = pyplot.subplots(ncols=n_cols, sharey='all', sharex='all')
+    axs[0] = display.property_map(g_ref_1, prop=mtg_property, ax=axs[0], prop2='Eabs', colormap='autumn')
+    axs[0].set_title(training_system_1)
+    axs[1] = display.property_map(g_ref_2, prop=mtg_property, ax=axs[1], prop2='Eabs', colormap='autumn')
+    axs[1].set_title(training_system_2)
+    if training_system_user != 'none':
+        axs[2] = display.property_map(g_user, prop=mtg_property, ax=axs[2], prop2='Eabs', colormap='autumn')
+        axs[2].set_title(training_system_user)
+    return fig
+
+
+def display_water_use_efficiency(path_output_low: Path, path_output_high: Path) -> pyplot.Figure:
     df_output_low = read_csv(path_output_low, sep=';', decimal='.', index_col='Unnamed: 0')
     df_output_high = read_csv(path_output_high, sep=';', decimal='.', index_col='Unnamed: 0')
 
@@ -231,30 +356,68 @@ def display_water_use_efficiency(path_output_low: Path, path_output_high: Path):
     ax.set(ylabel='\n'.join(["Efficience de l'utilisation de l'eau", "$\mathregular{g_{CO_2}\/g^{-1}_{H_2O}}$"]))
     fig.tight_layout()
 
+    return fig
+
+
+def update_params(
+        path_default_params: Path, path_user_params: Path, stomatal_conductance_min: float, stomatal_psi50: float,
+        steepness: float):
+    with open(path_default_params, mode='r') as f:
+        params = load_json(f)
+    params['simulation']['sdate'] = "2012-08-01 09:00:00"
+    params['simulation']['edate'] = "2012-08-01 15:00:00"
+    params['exchange']['par_gs']['g0'] = stomatal_conductance_min * 1.e-3
+    params['exchange']['par_gs']['psi0'] = stomatal_psi50
+    params['exchange']['par_gs']['n'] = steepness
+    with open(path_user_params, mode='w') as f:
+        dump(params, f, indent=2)
     pass
+
+
+def plot_stomatal_reduction_coef(psi50: float, gs0: float, n: float) -> pyplot.Figure:
+    gs0 /= 0.4
+    fig, ax = pyplot.subplots()
+    psi_leaf = [v / 1000 for v in range(-4000, 0, 10)]
+    gs_ref = [exchange.fvpd_3(model='misson', psi=v, vpd=None, psi_crit=-1, steepness_tuzet=4, m0=1) + gs0
+              for v in psi_leaf]
+    gs = [exchange.fvpd_3(model='misson', psi=v, vpd=None, psi_crit=psi50, steepness_tuzet=n, m0=1) + gs0
+          for v in psi_leaf]
+    ax.plot(psi_leaf, gs_ref, label='ref', color='orange')
+    ax.plot(psi_leaf, gs, label='usr', color='blue')
+    ax.vlines(x=-1, ymin=0, ymax=gs0 + 0.5, color='orange', linestyles='--')
+    ax.vlines(x=psi50, ymin=0, ymax=gs0 + 0.5, color='r', linestyles='--')
+    ax.hlines(y=0.02 / 0.4, xmin=min(psi_leaf), xmax=max(psi_leaf), color='orange', linestyles='--')
+    ax.hlines(y=gs0, xmin=min(psi_leaf), xmax=max(psi_leaf), color='r', linestyles='--')
+    ax.set_yticks([0, gs0, 0.5 + gs0, max(gs)])
+    ax.set_yticklabels(['0', 'cond. res.', 'cond. @ 50%', 'cond. max.'])
+    ax.set_xlabel('Potentiel xylémien (MPa)')
+    fig.tight_layout()
+    return fig
 
 
 if __name__ == '__main__':
     path_data = Path(__file__).parent / 'data'
     # generate_grapevine_mtgs(path_dir_project=path_data)
+    generate_grapevine_mtgs2(path_dir_project=path_data)
     # %gui qt5
 
-    # for is_low_area in (True, False):
-    #     vine_mtg, output = run_hydroshoot(
-    #         path_dir_preprocessed_inputs=path_data / 'preprocessed_inputs',
-    #         is_low_leaf_area=is_low_area,
-    #         is_show=True,
-    #         path_output=path_data / f'output_{"low" if is_low_area else "high"}/time_series.csv')
-
-    fig1 = display_whole_plant(
-        path_output_low=path_data / 'output_low/time_series.csv',
-        path_output_high=path_data / 'output_high/time_series.csv',
-        path_weather=path_data / 'weather.csv')
-
-    display_mtg_properties(
-        path_output_low=path_data / 'output_low',
-        path_output_high=path_data / 'output_high')
-
-    display_water_use_efficiency(
-        path_output_low=path_data / 'output_low/time_series.csv',
-        path_output_high=path_data / 'output_high/time_series.csv')
+    for training_system in ('cordon', 'lyre', 'gdc', 'vsp'):
+        vine_mtg, output = run_hydroshoot(
+            path_project=path_data,
+            path_dir_preprocessed_inputs=path_data / 'preprocessed_inputs',
+            training_sys=training_system,
+            is_show=False,
+            path_output=path_data / f'output_ref/{training_system}/time_series.csv')
+    #
+    # fig1 = display_whole_plant(
+    #     path_output_low=path_data / 'output_low/time_series.csv',
+    #     path_output_high=path_data / 'output_high/time_series.csv',
+    #     path_weather=path_data / 'weather.csv')
+    #
+    # display_mtg_properties(
+    #     path_output_low=path_data / 'output_low',
+    #     path_output_high=path_data / 'output_high')
+    #
+    # display_water_use_efficiency(
+    #     path_output_low=path_data / 'output_low/time_series.csv',
+    #     path_output_high=path_data / 'output_high/time_series.csv')
